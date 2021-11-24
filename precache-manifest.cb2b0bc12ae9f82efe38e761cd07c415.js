d1c903d96466724de9e31b664feff10bc593dbce
mnaga <mennat.abuelnaga@gmail.com>
2021-11-25 00:45:18 +0200

    Updates

---
+++ precache-manifest.cb2b0bc12ae9f82efe38e761cd07c415.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0537fe97a42968f19760f1103406bf57",
+    "url": "/ConsensysBootcampFinalProject/index.html"
+  },
+  {
+    "revision": "ffaae866e880423f334d",
+    "url": "/ConsensysBootcampFinalProject/static/css/2.98b51326.chunk.css"
+  },
+  {
+    "revision": "1b988be136d54ab0e6ad",
+    "url": "/ConsensysBootcampFinalProject/static/css/main.06af6865.chunk.css"
+  },
+  {
+    "revision": "ffaae866e880423f334d",
+    "url": "/ConsensysBootcampFinalProject/static/js/2.08f01f73.chunk.js"
+  },
+  {
+    "revision": "1b988be136d54ab0e6ad",
+    "url": "/ConsensysBootcampFinalProject/static/js/main.f9aff227.chunk.js"
+  },
+  {
+    "revision": "b27004c2a1c923bf2ff4",
+    "url": "/ConsensysBootcampFinalProject/static/js/runtime-main.260f7dbe.js"
+  },
+  {
+    "revision": "8c70efaef650527bc44a151026cda507",
+    "url": "/ConsensysBootcampFinalProject/static/media/wallpaper.8c70efae.jpg"
+  }
+]);
